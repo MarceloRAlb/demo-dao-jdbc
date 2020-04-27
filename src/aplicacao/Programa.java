9defaee801d4da1b39c3cc14dbb475b053a76799
MarceloAlbuquerqueTI <marcelotoberto.info@gmail.com>
2020-04-26 23:05:35 -0300

    Departament entity class

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,14 @@
+package aplicacao;
+
+import model.entidade.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+	
+		Departamento obj = new Departamento(1, "periféricos");
+		System.out.println(obj);
+
+	}
+
+}
